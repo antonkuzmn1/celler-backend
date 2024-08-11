0594ca80a8e55a4a46b3966ec99cedb298dbbbe9
anton <antonkuzmn1@gmail.com>
2024-08-11 21:27:13 +0700

    remove unused imports

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,6 @@
 import {Router} from 'express';
 import {securityController} from './security/security.controller';
-import {userController} from "./security/user/user.controller";
-import {groupController} from "./security/group/group.controller";
 import {tableController} from "./table/table.controller";
-import {columnController} from "./table/column.controller";
-import {rowController} from "./table/row.controller";
-import {cellController} from "./table/cell.controller";
 import {logController} from "./logger/log.controller";
 
 export const router = Router();
