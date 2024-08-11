ee2a937e901a88c0a842aa5dd84a93fb31908139
anton <antonkuzmn1@gmail.com>
2024-08-11 21:25:03 +0700

    improve startup

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import express from 'express';
+import express, {Express} from 'express';
 import cors from 'cors';
 import {router} from './router';
-import {createRootIfNotExists} from "./startup";
+import {OnStartupActions} from "./onStartupActions";
 import {logger} from "./logger/logger";
 
-const app = express();
+const app: Express = express();
 app.use(express.json());
 app.use(cors());
 app.use('/api', router);
 
-const PORT = process.env.PORT || 3000;
+const PORT: string | 3000 = process.env.PORT || 3000;
 app.listen(PORT, async () => {
     logger.info(`Server running on http://localhost:${PORT}`);
-    await createRootIfNotExists();
+
+    const startup: OnStartupActions = new OnStartupActions();
+    await startup.runAll();
 });
